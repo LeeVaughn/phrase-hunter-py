049e9570e3137183fb14985e09c842e1c6d101cf
LeeVaughn <dlvprogramming@gmail.com>
2020-05-23 09:39:36 -0700

    added comments to character.py for how to approach the class

--- phrasehunter/character.py
+++ phrasehunter/character.py
@@ -1,3 +1,19 @@
 # Create your Character class logic in here.
 
 # Character class will help a Phrase determine how a character should be displayed
+class Character:
+    def __init__(self, char, was_guessed = False):
+        # when instance is created make sure only a single character is passed w/ len(char) == 1
+        # else handle exception
+        original = char
+
+    def update_was_guessed(guess):
+        # update was_guessed to True if guess is exact match to the original char
+        # if guess == char
+            # set was_guessed to True
+
+    def show_char():
+        # if was_guessed is True
+            # show original char
+        # else
+            # show an underscore character
