b15765fdb1915af5124b248304e0f26b17a333e7
LeeVaughn <dlvprogramming@gmail.com>
2020-07-27 15:43:52 -0700

    imported classes and created new instances of Phrase and Game classes

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 # will contain the main logic for the app
+# from /phrasehunter import game
+# from /phrasehunter import phrase
+from phrasehunter.game import Game
+from phrasehunter.phrase import Phrase
+
+newPhrase = Phrase()
+newGame = Game()
 
 #* original comments below
 # Import your Game class
