93f859cb1a47ca021397a4a02c11e37d8093babb
LeeVaughn <dlvprogramming@gmail.com>
2020-08-01 09:42:03 -0700

    created and tested display method

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -3,6 +3,13 @@ class Phrase:
     def __init__(self, phrase):
         self.phrase = phrase.lower()
 
+    def display(self, guesses):
+        for letter in self.phrase:
+            if letter in guesses:
+                print(f"{letter}", end = " ")
+            else:
+                print("_ ", end = " ")
+
 #* original comments below
 # Create your Phrase class logic here.
 
