d33484f782ca578a3d578133b73d03681ea20fdb
LeeVaughn <dlvprogramming@gmail.com>
2020-08-03 11:03:11 -0700

    added docstring and cleaned up formatting

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,9 +1,19 @@
 from phrasehunter.phrase import Phrase
 import random
 
-# will contain the Game class and manage the game functionality
-# will contain methods to show the game, handle interactions, and check when the game is over
+
 class Game:
+    """
+    initializes the instance attributes and contains methods for gameplay
+
+    initializes the instance attributes
+    start method calls welcome method and continues gameplay until phrase is guessed or player is out of tries
+    welcome method prints welcome message
+    create_phrases method returns five Phrase objects
+    get_random_phrase method returns a random Phrase object
+    get_guess method prompts player to guess a letter
+    game_over method prints a win or loss message based on number of misses
+    """
     def __init__(self):
         self.missed = 0
         self.phrases = self.create_phrases()
@@ -29,13 +39,7 @@ class Game:
 
         self.game_over()
 
-    
     def welcome(self):
-        # print("\n=======================================")
-        # print("\n       Welcome to Phrase Hunter!")
-        # print("\n   Guess all of the letters and win.")
-        # print("\n              Good luck!")
-        # print("\n=======================================")
         print("""
         =======================================
 
@@ -73,15 +77,3 @@ class Game:
             print("Sorry, you have run out of guesses. Better luck next time!")
         else:
             print("Congratulations! You have guessed all of the letters in the phrase!")
-
-#* original comments below
-# Create your Game class logic in here.
-
-# game class will manage things like starting the game, handling interactions, getting random phrase, checking win/loss and removing lives
-
-# at the beginning of the the player will see the number of words and letters in the phrase represented by underscores
-# players will input a guess
-# once a correct letter is guessed it cannot be guessed again
-# if letter is in the phrase the _ will be replaced in all positions by the letter
-# if letter is not in the phrase the player will lose a life
-# play continues until the player has guessed all of the letters or run out of lives
